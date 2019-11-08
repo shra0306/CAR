351bf81211b9809b750de92dd2ee58ae0bee2ae8
ashw880591 <ecs.ashwini@gmai.com>
2019-11-08 16:55:22 +0530

    add project

--- src/app/set-password/set-password.component.ts
+++ src/app/set-password/set-password.component.ts
@@ -1,9 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-set-password',
   templateUrl: './set-password.component.html',
   styleUrls: ['./set-password.component.css']
 })
-export class SetPasswordComponent  {
-  constructor() { }
+export class SetPasswordComponent {
+  setPasswordModel :FormGroup;
+
+  constructor(private set:FormBuilder) {
+  
+    this.setPasswordModel =this.set.group({
+      enterpassword :[''],
+      confirmpassword:['']
+      
+    })
+    
+   }
+   onlClick(setpass){
+    alert(JSON.stringify(setpass))
+   }
+  
 }
+
